7444921fa204c69a034eb6be6202345c7ace59a7
user1 <user1@DESKTOP-5VTFQIT>
2024-05-23 09:01:31 +0900

    [ADD] 24/05/23 Github push 테스트
    1. Github repository 생성
    2. Github personal token 생성
    3. 테스트로 만든 코드를 Commit
    4. Commit을 github의 repository로 push

---
+++ src/basic/TestClass2.java
@@ -0,0 +1,36 @@
+package basic;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int[] arr = {1, 2, 3, 4, 5};
+//		
+//		int[] answer = new int[arr.length];
+//		for(int i=0; i<arr.length; i++){
+//            if (arr[i]>=50 && arr[i]%2==0)
+//                answer[i] = arr[i]/2;
+//            else if (arr[i]<50 && arr[i]%2==1)
+//                answer[i] = arr[i]*2;
+//            else
+//            	answer[i] = arr[i];
+//        }
+//		for(int num : answer)
+//			System.out.println(num+" ");
+		
+		
+		
+		String tString = "teST";
+		for(int i=0; i<tString.length();i++) {
+			if(64<tString.charAt(i) && tString.charAt(i)<91)
+				System.out.println(tString.charAt(i));
+				//tString.
+				//tString.charAt(i) += 32;
+				
+			//System.out.print(ch);
+		}
+		
+		
+	}
+
+}
